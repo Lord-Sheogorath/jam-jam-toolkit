f1b96e151b258234fdbeb5492c6464eb7d7a8ee7
Jack Smith-Hocking <hockingjack818@gmail.com>
2026-07-28 20:36:44 +0930

    Added extensions from toolkit package

---
+++ Runtime/Scripts/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+
+namespace LordSheo.JJTK
+{
+	public static class EnumerableExtensions
+	{
+		public static bool IsNullOrEmpty<T>(this ICollection<T> source)
+		{
+			return source == null || source.Count == 0;
+		}
+
+		public static void Perform<T>(this IEnumerable<T> source, Action<T> action)
+		{
+			foreach (var item in source)
+			{
+				action.Invoke(item);
+			}
+		}
+
+		public static T Random<T>(this IList<T> source)
+		{
+			var index = UnityEngine.Random.Range(0, source.Count - 1);
+			return source[index];
+		}
+		public static void AddRange<T>(HashSet<T> source, IEnumerable<T> collection)
+		{
+			foreach (var elem in collection)
+			{
+				source.Add(elem);
+			}
+		}
+
+		public static TValue GetOrAddDefault<TKey, TValue>(this IDictionary<TKey, TValue> source, TKey key, TValue defaultValue = default)
+		{
+			if (source.TryGetValue(key, out var value) == false)
+			{
+				source[key] = value = defaultValue;
+			}
+
+			return value;
+		}
+	}
+}
