767bd7a5786f81d14657e62d9f088b0140c47e02
Jack Smith-Hocking <hockingjack818@gmail.com>
2026-07-29 22:00:58 +0930

    Added ScriptableObjectSingleton

---
+++ Runtime/Scripts/Utility/ScriptableObjectSingleton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace LordSheo.JJTK
+{
+	public class ScriptableObjectSingleton<T> : ScriptableObject
+		where T : ScriptableObject
+	{
+		public static T Instance => Load();
+		private static T _instance;
+
+		private static T Load()
+		{
+			if (_instance == null)
+			{
+				_instance = Resources.Load<T>("Singletons/" + nameof(T));
+			}
+
+			return _instance;
+		}
+	}
+}
