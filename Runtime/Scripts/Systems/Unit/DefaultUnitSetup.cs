776c80a1efab6b213446c5ed60b6b421875343f8
Jack Smith-Hocking <hockingjack818@gmail.com>
2026-07-31 22:14:10 +0930

    Added DefaultUnitSetup as separate file

---
+++ Runtime/Scripts/Systems/Unit/DefaultUnitSetup.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+namespace LordSheo.JJTK
+{
+	public class DefaultUnitSetup : MonoBehaviour
+	{
+		private UnitController _unit;
+		
+		public void Awake()
+		{
+			_unit = GetComponent<UnitController>();
+			
+			var statSystem = new DefaultStatSystem();
+			var inventorySystem = new DefaultInventorySystem();
+			var healthSystem = new DefaultHealthSystem(statSystem);
+			var targetSystem = new DefaultTargetSystem();
+			var actionSystem = new DefaultUnitActionSystem(_unit);
+			var combatSystem = new DefaultCombatSystem();
+			var signalSystem = new DefaultSignalSystem();
+			
+			_unit.AddSystem<IStatSystem, DefaultStatSystem>(statSystem);
+			_unit.AddSystem<IInventorySystem, DefaultInventorySystem>(inventorySystem);
+			_unit.AddSystem<IHealthSystem, DefaultHealthSystem>(healthSystem);
+			_unit.AddSystem<ITargetSystem, DefaultTargetSystem>(targetSystem);
+			_unit.AddSystem<IUnitActionSystem, DefaultUnitActionSystem>(actionSystem);
+			_unit.AddSystem<ICombatSystem, DefaultCombatSystem>(combatSystem);
+			_unit.AddSystem<ISignalSystem, DefaultSignalSystem>(signalSystem);
+		}
+	}
+}
--- Runtime/Scripts/Systems/Unit/UnitController.cs
+++ Runtime/Scripts/Systems/Unit/UnitController.cs
@@ -4,31 +4,6 @@ using UnityEngine;
 
 namespace LordSheo.JJTK
 {
-	public class DefaultUnitSetup : MonoBehaviour
-	{
-		private UnitController _unit;
-		
-		public void Awake()
-		{
-			_unit = GetComponent<UnitController>();
-			
-			var statSystem = new DefaultStatSystem();
-			var inventorySystem = new DefaultInventorySystem();
-			var healthSystem = new DefaultHealthSystem(statSystem);
-			var targetSystem = new DefaultTargetSystem();
-			var actionSystem = new DefaultUnitActionSystem(_unit);
-			var combatSystem = new DefaultCombatSystem();
-			var signalSystem = new DefaultSignalSystem();
-			
-			_unit.AddSystem<IStatSystem, DefaultStatSystem>(statSystem);
-			_unit.AddSystem<IInventorySystem, DefaultInventorySystem>(inventorySystem);
-			_unit.AddSystem<IHealthSystem, DefaultHealthSystem>(healthSystem);
-			_unit.AddSystem<ITargetSystem, DefaultTargetSystem>(targetSystem);
-			_unit.AddSystem<IUnitActionSystem, DefaultUnitActionSystem>(actionSystem);
-			_unit.AddSystem<ICombatSystem, DefaultCombatSystem>(combatSystem);
-			_unit.AddSystem<ISignalSystem, DefaultSignalSystem>(signalSystem);
-		}
-	}
 	public class UnitController : MonoBehaviour
 	{
 		[SerializeField]
