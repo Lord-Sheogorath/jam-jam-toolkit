a85a1634b14516343eaf99de53a45fa5954de3c7
Jack Smith-Hocking <hockingjack818@gmail.com>
2026-07-31 22:14:26 +0930

    Added UnitControllerLogger to log unit events

---
+++ Runtime/Scripts/Systems/Unit/UnitControllerLogger.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace LordSheo.JJTK
+{
+	public class UnitControllerLogger : MonoBehaviour
+	{
+		private UnitController _unit;
+		
+		private void Awake()
+		{
+			_unit = GetComponent<UnitController>();
+		}
+
+		private void Start()
+		{
+			var statSystem = _unit.GetSystem<IStatSystem>();
+			statSystem.OnAddedEvent += (type, amount) => Log($"[Unit/Stat] Added, {amount}, {type}");
+			statSystem.OnRemovedEvent += (type, amount) => Log($"[Unit/Stat] Removed, {amount}, {type}");
+			statSystem.OnChangedEvent += (type, amount) => Log($"[Unit/Stat] Changed, {amount}, {type}");
+
+			var inventorySystem = _unit.GetSystem<IInventorySystem>();
+			inventorySystem.OnAddedEvent += (type, amount) => Log($"[Unit/Inventory] Added, {amount}, {type}");
+			inventorySystem.OnRemovedEvent += (type, amount) => Log($"[Unit/Inventory] Removed, {amount}, {type}");
+			inventorySystem.OnChangedEvent += (type, amount) => Log($"[Unit/Inventory] Changed, {amount}, {type}");
+
+			var healthSystem = _unit.GetSystem<IHealthSystem>();
+			healthSystem.OnHealedEvent += (amount) => Log($"[Unit/Health] Healed, {amount}");
+			healthSystem.OnDamagedEvent += (amount) => Log($"[Unit/Health] Damaged, {amount}");
+			healthSystem.OnChangedEvent += () => Log($"[Unit/Health] Changed");
+			healthSystem.OnDeathEvent += () => Log($"[Unit/Health] Death");
+
+			var combatSystem = _unit.GetSystem<ICombatSystem>();
+			combatSystem.OnChangedEvent += (prev, current) => Log($"[Unit/Combat] Changed, {prev}, {current}");
+		}
+
+		private void Log(string message)
+		{
+			Debug.Log(message, this);
+		}
+	}
+}
