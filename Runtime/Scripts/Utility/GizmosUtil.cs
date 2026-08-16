3d3c820d5c8fc9d4843ef5ef1b835c30ce694206
Jack Smith-Hocking <hockingjack818@gmail.com>
2026-08-16 10:48:12 +0930

    Added GizmosUtil

---
+++ Runtime/Scripts/Utility/GizmosUtil.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace LordSheo.JJTK
+{
+	public static class GizmosUtil
+	{
+		private static readonly Queue<Color> _colorScopes = new();
+
+		public static void StartColorScope(Color color)
+		{
+			_colorScopes.Enqueue(Gizmos.color);
+
+			Gizmos.color = color;
+		}
+
+		public static void EndColorScope()
+		{
+			if (_colorScopes.Count == 0)
+			{
+				Debug.LogError("Must call StartColorScope before EndColorScope.");
+				return;
+			}
+			
+			Gizmos.color = _colorScopes.Dequeue();
+		}
+	}
+}
